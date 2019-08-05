26d6efc79ecf1ae120a72cb38bbff9e02f825619
Anne Marie O'Sullivan <annemarieos.irl@gmail.com>
2019-08-05 13:27:36 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
+
+function greet(time_string) {
+  const hour = parseInt(time_string, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+    return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+
